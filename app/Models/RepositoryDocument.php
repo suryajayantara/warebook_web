3f0e92134fa149bb13f496e17b5091fb2e07cdcf
suryajayantara <ptsuryajayantara01@gmail.com>
2022-06-10 23:35:59 +0800

    :sparkles: Init Repository

---
+++ app/Models/RepositoryDocument.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RepositoryDocument extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2022_06_10_153138_create_repositories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRepositoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('repositories', function (Blueprint $table) {
+            $table->id();
+            $table->string('repository_name');
+            $table->string('unique_code');
+            $table->foreignId('id_users');
+            $table->foreignId('type_of_repository');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('repositories');
+    }
+}
