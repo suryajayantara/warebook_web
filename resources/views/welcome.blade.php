1c8b30075ad9623ef00f2427229d94eabf23538c
lordarya <12aryacandrayana@gmail.com>
2022-07-15 21:57:02 +0800

    :spark: create dashboard user view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hello</h1>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('contents')
+    <div class="bg-[#FBFDFF] h-80 w-full">
+        <div class="container w-[75%] flex mx-auto items-center h-80 z-10">
+            <div>
+                <h1 class="text-6xl font-extrabold">Temukan Inspirasimu</h1>
+                <p class="mx-5">Temukan ide ide brilian dari apa yang telah dibuat oleh kakak kakakmu terdahulu</p>
+            </div>
+            <div class="">
+                <img class="h-72 ml-40 " src="{{asset('img/backdrop.svg')}}" alt="">
+            </div>
+        </div>
+        <div class="flex items-center shadow-lg h-16 w-[75%] bg-[#EEEFF3] mx-auto  rounded-md -mt-9 z-20">
+            <img class="h-10 mx-5" src="{{asset('img/icon/search.svg')}}" alt="">
+            <input class="bg-[#EEEFF3] h-16 w-full" placeholder="Cari Judul Repositori Disini" type="text" name="" id="">
+        </div>
+    </div>
+@endsection
