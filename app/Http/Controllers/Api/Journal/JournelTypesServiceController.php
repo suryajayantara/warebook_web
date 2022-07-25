d022eed1d993a553fe56024a17460161780418b0
adearta <bagusmadearta01@gmail.com>
2022-07-25 14:21:37 +0800

    test

---
+++ app/Http/Controllers/Api/Journal/JournelTypesServiceController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\Journal;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class JournelTypesServiceController extends Controller
+{
+    //
+}
