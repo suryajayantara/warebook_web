091aad238e59214b1059111b6d9c3a6de816cb13
adearta <bagusmadearta01@gmail.com>
2022-07-30 09:57:46 +0800

    :tada: make route for CRUD repository web

--- app/Models/Departement.php
+++ app/Models/Departement.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Departement extends Model
 {
+    /**
+     * departement_name : digunakan untuk menginput data nama jurusan yang bertipe string
+     * desc : digunakan untuk menginput data deskripsi yang bertipe text
+     */
     use HasFactory;
     protected $fillable = [
         'departement_name',
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\Web\InternalResearch\InternalResearchController;
+use App\Http\Controllers\Web\Journal\JournalDocumentController;
+use App\Http\Controllers\Web\Journal\JournalTopicController;
+use App\Http\Controllers\Web\Journal\JournalTypeController;
 use App\Http\Controllers\Web\Manage\DepartementController;
 use App\Http\Controllers\Web\Manage\StudyController;
+use App\Http\Controllers\Web\StudentCreativityProgram\StudentCreativityProgramController;
+use App\Http\Controllers\Web\StudentCreativityProgram\StudentCreativityProgramTypeController;
+use App\Http\Controllers\Web\Thesis\ThesisController;
+use App\Http\Controllers\Web\Thesis\ThesisDocumentController;
 use App\Http\Controllers\Web\User\RegisterController;
+use App\Models\Thesis;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -32,3 +41,11 @@ Route::get('/home', function(){
 Route::resource('departements',DepartementController::class);
 Route::resource('studies',StudyController::class);
 Route::resource('register',RegisterController::class);
+Route::resource('journalDocument',JournalDocumentController::class);
+Route::resource('journalTopic',JournalTopicController::class);
+Route::resource('journalType',JournalTypeController::class);
+Route::resource('thesis',ThesisController::class);
+Route::resource('thesisDocument',ThesisDocumentController::class);
+Route::resource('creativity',StudentCreativityProgramController::class);
+Route::resource('creativityType',StudentCreativityProgramTypeController::class);
+Route::resource('internalResearch',InternalResearchController::class);
